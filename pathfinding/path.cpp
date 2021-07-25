19181072e56efd78c48fb24a38e3ff3d9ae06504
Dru Ellering <54593927+dru26@users.noreply.github.com>
2021-07-24 21:03:12 -0400

    save

---
+++ pathfinding/path.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include "../dynamic-map/position.h"
+#include "../dynamic-map/map.h"
+
+typedef std::vector<Position> Path;
+
+int main() {
+    std::priority_queue<int> mypq;
+    return EXIT_SUCCESS;
+}
+
+// implimentation of Dijkstra's algorithm
+void to(Position dest) {
+    start
+
+
+    std::priority_queue<Path> active;
+
+}
---
+++ pathfinding/path.h
@@ -0,0 +1,11 @@
+#include <vector>
+
+#include "../dynamic-map/position.h"
+
+// An immutable position containing an (x, y) pair
+class Path {
+public:
+
+private:
+    std::vector<Position> _path;
+};
