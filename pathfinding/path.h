0fdf3d36db6f294cd9719f4cf06916362c17b170
Dru Ellering <54593927+dru26@users.noreply.github.com>
2021-07-25 00:22:23 -0400

    Implemented Dijkstra's algorithm

--- pathfinding/path.h
+++ pathfinding/path.h
@@ -1,11 +1,29 @@
-#include <vector>
+#ifndef _path_h_
+#define _path_h_
 
 #include "../dynamic-map/position.h"
 
-// An immutable position containing an (x, y) pair
-class Path {
-public:
+typedef std::vector<Position> Path;
+Path EMPTY;
 
+class WeightedPath {
+public:
+    WeightedPath() : _weight(0) {}
+    void push_back(Position p, float w) { _path.push_back(p); _weight += w; }
+    int size() const { return this->_path.size(); }
+    float weight() const { return this->_weight; }
+    Path get() { return this->_path; }
+    void operator=(const WeightedPath& rhs) { this->_path = rhs._path; this->_weight = rhs._weight; }
 private:
-    std::vector<Position> _path;
+    Path _path;
+    float _weight;
 };
+
+class WeightedPathCompare {
+public:
+    bool operator() (const WeightedPath &a, const WeightedPath &b) {
+        return a.weight() > b.weight();
+    }
+};
+
+#endif
